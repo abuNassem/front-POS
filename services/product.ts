2cc8ebb46ea6c9baf958a6f796b66b6b23c8b35e
abuNassem <qutaibah0mohmmed@gmail.com>
2026-05-02 18:07:44 +0100

    Refactor getProduct to accept page parameterfi

--- services/product.ts
+++ services/product.ts
@@ -1,26 +1,19 @@
-<<<<<<< HEAD
-import { extraInfoProduct, Populated, Product } from "@/types/product"
-=======
+
 import { Populated, Product } from "@/types/product"
->>>>>>> 47f7505cb4d54d229acaadaebaacb604e90e97cd
+
 import apiClient from "./index"
 import { ProductImport } from "@/app/dashboard/Products/components/import"
 
-<<<<<<< HEAD
 export const getProduct = async (id: string | null,page?:number) => {
-=======
-export const getProduct = async (id: string | null) => {
->>>>>>> 47f7505cb4d54d229acaadaebaacb604e90e97cd
+
     if (id) {
         const res = await apiClient.get<Product>(`/product?id=${id}`)
         return res.data
     } else {
-<<<<<<< HEAD
+
         const res = await apiClient.get<Product[]>(`/product?page=${page}`)
         console.log('page',page)
-=======
-        const res = await apiClient.get<Product[]>("/product")
->>>>>>> 47f7505cb4d54d229acaadaebaacb604e90e97cd
+
         return res.data
     }
 
@@ -56,7 +49,6 @@ export const deleteProduct = (id: string) => {
 
 export const SearchProduct = (param: string | number) => {
     return apiClient.get(`product/search?q=${param}`)
-<<<<<<< HEAD
 }
 
 export const ImportProduct=(products:ProductImport[],mode:'strict'|'smart')=>{
@@ -67,6 +59,5 @@ export const deleteMany=(ids:string[])=>{
     if(Array.isArray(ids) && ids.length>1){
          return apiClient.post('/product/deleteMany',{ids})
     }
-=======
->>>>>>> 47f7505cb4d54d229acaadaebaacb604e90e97cd
+
 }
